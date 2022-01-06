052a2f0f559fa8074de9713022320c0e058f2630
mrj143 <97012824+mrj143@users.noreply.github.com>
2022-01-06 22:13:26 +0500

    Update UniswapV2Library.sol

--- UniswapV2Library.sol
+++ UniswapV2Library.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0;
 
 import './interfaces/IUniswapV2Pair.sol';
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 library UniswapV2Library {
     using SafeMath for uint;
